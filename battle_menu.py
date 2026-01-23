b3d45c516526ddd29225c8579df542f0552397be
scota <scota836@hsd.k12.or.us>
2026-01-23 09:23:10 -0800

    started my project

---
+++ battle_menu.py
@@ -0,0 +1,22 @@
+import random
+
+year = 1
+User_starting_HP = int(100)
+User_starting_attack = int(20)
+Enemy_starting_HP = int(80)
+Enemy_starting_damage = int(15)
+
+#different buffs peoploe can chose:
+
+User_buff = input(print("Chose a buff, extra health, extra damadge, or first attack next.(type in hp,dmg,atk)"))
+
+def time_changing_post_fight():
+    for year in range:
+        Enemy_starting_HP + int(20)
+        Enemy_starting_damage + int(10)
+
+new_atk = int(25)
+
+if User_buff == str(atk):
+    User_starting_attack - User_starting_attack + new_atk
+    print("Your attack buff now does" new_atk "instead of 20dmg")
