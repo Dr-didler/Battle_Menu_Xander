317e3bb20ec9b9ba106321281e219beffd9304fb
scota <scota836@hsd.k12.or.us>
2026-01-23 20:14:16 -0800

    oh yeah it workks, that was supa fast,
    the whille statement was ruineing everything so i replaced it, next i need to incorperate a users healing post fight, so far its going pretty smoothly

--- battle_menu.py
+++ battle_menu.py
@@ -11,54 +11,65 @@ Mini_boss_damage = int(45)
 Boss_health = int(350)
 Boss_regen = int(20)
 Boss_dmg = int(70)
-
-#different buffs peoploe can chose:
+ability_to_atk = 0
 
 
 
 #This function is for the time passing giving the enemys buffs
 
 def time_changing_post_fight():
-    for year in range:
-        Enemy_starting_HP + int(20)
-        Enemy_starting_damage + int(10)
+    global Enemy_starting_HP, Enemy_starting_damage
+    Enemy_starting_HP += 20
+    Enemy_starting_damage += 10
 
 def user_choice_upg():
+    global User_starting_HP, User_starting_attack, ability_to_atk
     User_buff = input("Chose a buff, extra health, extra damadge, or first attack next.(type in hp,dmg,atk)")
-    if User_buff == dmg:
-        User_starting_attack + int(5)
-        print("Your attack buff now does 25dmg instead of 20dmg")
-    elif User_buff == hp:
-        User_starting_HP + int(10)
-        print("Your new HP is 110")
-    elif User_buff == atk:
-        ability_to_atk = int(1)
+    if User_buff == "dmg":
+        User_starting_attack += 5
+        print("Your attack buff now does", User_starting_attack, "dmg.")
+    elif User_buff == "hp":
+        User_starting_HP += 10
+        print("Your new HP is", User_starting_HP)
+    elif User_buff == "atk":
+        ability_to_atk = 1
         print("you are going to attack first in your next encounter.")
+    else:
+        print("No buff applied.")
 #this random number is for the amount of enemys you will fight, the larger the number, the more buffs you can apply
 enemy_count = random.randint(1,5)
 
 
 
 def starting_sequence():
-   # if User_choice == 1:
-        print("ahead lay", enemy_count, "enemies, each current enemy has", Enemy_starting_HP, "you currently do", User_starting_attack, "per attack."  )
-        Enemy_starting_HP - User_starting_attack 
-        print("the enemy before you now has" , Enemy_starting_HP)
-        #add a loop here to keep fighting until all enemys are defeated
-        for enemy in range(enemy_count):
-            while Enemy_starting_HP > 0:
-                Enemy_starting_HP - User_starting_attack
-                print("the enemy before you now has" , Enemy_starting_HP)
-                if Enemy_starting_HP <= 0:
-                    print("you have defeated the enemy!")
-                    time_changing_post_fight()
-                    user_choice_upg()
-                    break
-                User_starting_HP - Enemy_starting_damage
-                print("you have", User_starting_HP, "left")
-                if User_starting_HP <= 0:
-                    print("you have been defeated, game over.")
-                    break
+    print("ahead lay", enemy_count, "enemies, each current enemy has", Enemy_starting_HP, "you currently do", User_starting_attack, "per attack.")
+    user_hp = User_starting_HP
+    user_attack = User_starting_attack
+    # add a loop here to keep fighting until all enemys are defeated
+    for enemy in range(enemy_count):
+        enemy_hp = Enemy_starting_HP
+        print("A new enemy appears with", enemy_hp, "HP.")
+        # replace while-loop with a bounded for-loop to avoid infinite loops
+        for turn in range(1000):
+            # player's attack
+            enemy_hp -= user_attack
+            print("the enemy before you now has", enemy_hp)
+            if enemy_hp <= 0:
+                print("you have defeated the enemy")
+                time_changing_post_fight()
+                user_choice_upg()
+                # refresh local stats in case buffs changed globals
+                user_attack = User_starting_attack
+                user_hp = User_starting_HP
+                break
+            # enemy attacks
+            user_hp -= Enemy_starting_damage
+            print("you have", user_hp, "left")
+            if user_hp <= 0:
+                print("you have been defeated, game over.")
+                return
+        else:
+            print("The battle reached the maximum number of rounds and ends.")
         
 User_choice = input("You walk into a coridor, enemy's lay ahead, time to fight.")
 starting_sequence()
